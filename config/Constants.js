d44f93852a9dd4877c46b0f5b3865c13d58eb8f2
JorEdHeS24 <166845897+JorEdHeS24@users.noreply.github.com>
2026-03-31 19:43:28 -0500

    first commit

---
+++ config/Constants.js
@@ -0,0 +1,71 @@
+export const CARD_TEXT_CONTAINER = (text)=>
+{
+    return `<p class="card-text">${text}</p>`
+}
+
+export const EXPIRIENCE_DETAILS = 
+[
+    {
+    position : "Agente de call center",  
+    enterprise : "Teleperformance", 
+    description : "Agente de Call center Bilingüe para Banco digital en estados unidos", 
+    duration : "Agosto 2025 - Enero 2026"  
+    },    
+    {
+    position : "Agente de call center",  
+    enterprise : "Sitel", 
+    description : "Agente de Call center Semibilingue para Verizon estados", 
+    duration : "Julio 2022 - Diciembre 2022"  
+    }
+]
+export const FORMATION_DETAILS = 
+[
+    {
+    degree : "Universidad / Carrera Profesional",  
+    place : "Corporacion universitaria Latinoamerica (CUL)",  
+    duration : "Enero 2024 - Actualmente"  
+    },       
+    {
+    degree : "Universidad / Carrera técnica",  
+    place : "ITSA/Uni-Barranquilla",  
+    duration : "Enero 2017 - Diciembre 2019"  
+    }   
+    
+]
+
+export const SOFT_SKILLS_TAGS = 
+[
+    "Trabajo en equipo", 
+    "Pensamiento crítico", 
+    "Resolución de problemas", 
+    "Pensamiento analítico", 
+    "Proactividad"
+]
+
+export const WORK_SKILLS_TAGS = 
+[
+    "Programacion", 
+    "Desarrollo web", 
+    "Desarrollo Full Stack", 
+    "React", 
+    "Python", 
+    "HTML",
+    "CSS",
+    " Javascript", 
+    "Sql",
+    "Api rest",
+    "Java",
+    "Inteligencia artificial",
+    "C#",
+    ".net",
+    "Php",
+    "Scrum",
+    "Uml",
+    "Mvc",
+    "Mantenimiento de computadores",
+    "Servicio al cliente",
+    "Servicio al cliente"
+]
+    
+
+
---
+++ script.js
@@ -0,0 +1,77 @@
+import { 
+    CARD_TEXT_CONTAINER, 
+    EXPIRIENCE_DETAILS, 
+    FORMATION_DETAILS} from "./config/Constants.js"
+import { SOFT_SKILLS_TAGS, WORK_SKILLS_TAGS } from "./config/Constants.js"
+
+
+
+// BACKGROUND SUMMARY CARD
+
+const expirienceList = document.querySelector(".expirience-list")
+const formationList = document.querySelector(".formation-list")
+const skillsContainer = document.querySelector(".skills-content")
+const workSkillContainer = document.querySelector(".work-skills-tags")
+
+const BgrSummaryCard = (section_content, card_text_container, list_element)=>{
+    for(let detail in section_content)
+    {
+        let details = Object.values(section_content[detail])
+        console.log(details)    
+        
+        let summaryCard = document.createElement("li")
+        summaryCard.classList.add("summary-card-details","ornament-before", "ornament-after") 
+        
+        for(let detail of details)
+        {            
+            summaryCard.innerHTML += card_text_container(detail)        
+        }
+        
+        list_element.appendChild(summaryCard)
+
+    }
+     
+}
+
+function addLink(HTML_Element, text)
+{
+    let addLink = document.createElement("li")
+    let link = document.createElement("a")
+    link.href = "#"
+    link.classList.add("ornament-before")
+    link.textContent = `${text}`
+    addLink.classList.add("add-link-style")
+    addLink.appendChild(link)
+    HTML_Element.append(addLink)   
+}
+
+let softSkillsTags  = SOFT_SKILLS_TAGS
+let workSkillsTags  = WORK_SKILLS_TAGS
+
+function tagCreate(container, text)
+{    
+    return container.innerHTML += `<p class="tag">${text}</p>`
+}
+
+function addNewTag(containerElement, tagCreateContainer,softSkillsTags)
+{
+    for (let skill in softSkillsTags)
+    {
+            tagCreate(tagCreateContainer, softSkillsTags[skill])
+    }
+    let buttonAdd = document.createElement("button")
+    buttonAdd.textContent ="Add +"
+    buttonAdd.classList.add ="AddTagButton"
+    containerElement.append(buttonAdd)
+
+}
+
+BgrSummaryCard(EXPIRIENCE_DETAILS, CARD_TEXT_CONTAINER, expirienceList)
+BgrSummaryCard(FORMATION_DETAILS, CARD_TEXT_CONTAINER, formationList)
+
+addLink(expirienceList, "Add Expirience")
+addLink(formationList, "Add Formation")
+
+addNewTag(skillsContainer, skillsContainer, softSkillsTags)
+addNewTag(workSkillContainer, workSkillContainer, workSkillsTags)
+// tagCreate(skillsContainer, "Proactivo".trim())
